7f3aebe6c3aa354116f40d87a1108a5859b1f439
JesianeGoedert <jesiane.goedert@gmail.com>
2021-11-19 10:58:46 -0300

    up

---
+++ fdata.php
@@ -0,0 +1,14 @@
+<?php
+    function dataFormat($data){
+        return date("d/m/Y",strtotime($data));
+}
+
+    //function intervalo($dataatual, $contratacao) {
+    //    $d1= strtotime($dataatual);
+    //    $d2= strtotime($contratacao);
+    //   $intervalo = ($d1- $d2)-86400;
+    //   return date('d');
+    //}
+    //echo $intervalo 
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php 
+     include_once "conf/default.inc.php";
+     include_once "fdata.php";
+     require_once "conf/Conexao.php";
+     $title = "Vendas";
+     $tipo = isset($_POST['tipo']) ? $_POST['tipo'] : "1";
+     $procurar = isset($_POST['procurar']) ? $_POST['procurar'] : "";
+?>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title> <?php echo $title; ?> </title>
+</head>
+<body>
+
+<form method="post">
+    <input type="radio" name="tipo" id="tipo" value="1" <?php if ($tipo == 1) { echo "checked"; }?>>Nome<br>  
+    <input type="radio" name="tipo" id="tipo" value="2" <?php if ($tipo == 2) { echo "checked"; }?>>Fixo<br>
+    <input type="text" name="procurar" id="procurar" value="<?php echo $procurar; ?>">
+    <input type="submit" value="Consultar">
+</form>
+<br>
+<?php
+    $sql = "";
+    if ($tipo == 1){
+        $sql = "SELECT * FROM venda WHERE nome LIKE '$procurar%' ORDER BY nome";
+    }else if($tipo ==2){    
+        $sql = "SELECT * FROM venda WHERE fixo <= '$procurar%' ORDER BY fixo";
+    }
+    
+    $pdo = Conexao::getInstance(); 
+    $consulta = $pdo->query($sql);
+    while ($linha = $consulta->fetch(PDO::FETCH_BOTH)){
+        echo "Nome: {$linha['nome']} - Fixo: {$linha['fixo']} - <a href='venda-detalhes.php?id={$linha['id']}'>Detalhes</a><br/>";
+    }
+?>
+</body>
+</html>
